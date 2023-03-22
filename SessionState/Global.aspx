a9d0deee1b8d01ca751225c44ecd2bda6d60577a
DuBronC <tzenyangc@gmail.com>
2023-03-23 00:31:41 +0800

    Practical  : Exercise not complete

---
+++ SessionState/Global.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Global.aspx.cs" Inherits="SessionState.Global" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link id="css" href="<%=(string)Session["SelectedCss"] %>" type="text/css" rel="stylesheet" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            You are visitor:
+            <asp:Label ID="lblVisitCount" runat="server"></asp:Label>
+            <br />
+            You access on
+            <asp:Label ID="lblVisitTime" runat="server"></asp:Label>
+            <br />
+            <br />
+            Choose a theme:</div>
+        <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True">
+            <asp:ListItem Value="Standard"></asp:ListItem>
+            <asp:ListItem Value="Special"></asp:ListItem>
+        </asp:DropDownList>
+        <asp:Button ID="Button1" runat="server" Text="Apply" />
+        <br />
+        <br />
+        <asp:CheckBox ID="CheckBox1" runat="server" OnCheckedChanged="CheckBox1_CheckedChanged" Text="Remember preference" />
+    </form>
+</body>
+</html>
---
+++ SessionState/Global.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SessionState
+{
+    public partial class Global : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session["visitorsCount"] == null)
+                {
+                    // Set initial value
+                    Session["visitorsCount"] = 1;
+                }
+                else
+                {
+                    // Increment value
+                    Session["visitorsCount"] = Convert.ToInt32(Session["visitorsCount"]) + 1;
+                }
+
+                // Display the number of visitors
+                lblVisitCount.Text = Session["visitorsCount"].ToString();
+
+                if (Session["lastVisitTime"] == null)
+                {
+                    // Set initial value
+                    Session["lastVisitTime"] = DateTime.Now;
+                }
+                else
+                {
+                    // Display the last visit time
+                    DateTime lastVisitTime = (DateTime)Session["lastVisitTime"];
+                    lblVisitTime.Text = lastVisitTime.ToString();
+                }
+
+                // Update the visit time
+                Session["lastVisitTime"] = DateTime.Now;
+            }
+
+
+        }
+    }
+}
